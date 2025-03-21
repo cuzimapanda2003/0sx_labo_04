cde9b23c8965cdf702c7f9798bacc07df4e20e0d
cuzimapanda2003 <marcblais03@gmail.com>
2025-03-21 10:55:53 -0400

    Update sketch_mar21a.ino

--- sketch_mar21a/sketch_mar21a.ino
+++ sketch_mar21a/sketch_mar21a.ino
@@ -1,5 +1,21 @@
+#include <AccelStepper.h>
+
+#define MOTOR_INTERFACE_TYPE 4
+
+#define IN_1 8
+#define IN_2 9
+#define IN_3 10
+#define IN_4 11
+
+#define TRIGGER_PIN 21
+#define ECHO_PIN 20
+
+
+AccelStepper myStepper(MOTOR_INTERFACE_TYPE, IN_1, IN_3, IN_2, IN_4);
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
 }
 
